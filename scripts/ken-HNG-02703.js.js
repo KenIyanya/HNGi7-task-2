94db9301c23287feff4a96c6d09f89fd6e3390ff
ken <iyanyakenneth@gmail.com>
2020-06-05 20:55:39 +0100

    ken-HNG-02702 script updated

--- scripts/ken-HNG-02703.js.js
+++ scripts/ken-HNG-02703.js.js
@@ -1,11 +1,7 @@
 const name = "Iyanya Kenneth";
 const HNGi7_ID = "HNG-02703";
 const language = "JavaScript";
-<<<<<<< HEAD
 const email  = "iyanyakenneth@gmail.com";
-=======
-const email  = "iyanyakenneth@gmail.com"
->>>>>>> a668461de27c78a9de110d495db6bb253460782e
 
 const string = () =>{
   return `Hello World, this is ${name} with HNGi7 ID ${HNGi7_ID} and email ${email} using ${language} for stage 2 task.`};
